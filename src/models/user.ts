db4d6a50d202932cb4b34d6cf617c4189792dfad
iha7 <105142183+iha7@users.noreply.github.com>
2023-03-31 01:36:22 -0500

    Added Controllers, Updated User and added list Model, and added Auth Services and auth through bcrypt

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,11 +1,43 @@
-export class User {
-    id: number;
-    email: string;
-    name: string;
+import { DataTypes, InferAttributes, InferCreationAttributes, Model, Sequelize } from "sequelize";
 
-    constructor (id: number, email: string, name: string) {
-        this.id = id;
-        this.email = email;
-        this.name = name;
-    }
+export class User extends Model<InferAttributes<User>, InferCreationAttributes<User>>{
+    declare userId: number;
+    declare username: string;
+    declare password: string;
+    declare createdAt?: Date;
+    declare updatedAt?: Date;
+}
+
+export function UserFactory(sequelize: Sequelize) {
+    User.init({
+        userId: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+            allowNull: false
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        createdAt: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        },
+        updatedAt: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        }
+    }, {
+        tableName: 'users',
+        freezeTableName: true,
+        sequelize
+    });
 }
