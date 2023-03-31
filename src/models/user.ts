52c416daa174724f73e48b00f64c8b7cef857af8
Emmanuel-Fraire <107206275+Emmanuel-Fraire@users.noreply.github.com>
2023-03-30 19:21:51 -0500

    user model

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,11 +1,44 @@
-export class User {
-    id: number;
-    email: string;
-    name: string;
+import { DataTypes, InferAttributes, InferCreationAttributes, Model, Sequelize } from "sequelize";
 
-    constructor (id: number, email: string, name: string) {
-        this.id = id;
-        this.email = email;
-        this.name = name;
-    }
+export class User extends Model<InferAttributes<User>, InferCreationAttributes<User>>{
+    declare userName: string;
+    declare email: string;
+    declare password: string;
+    declare createdAt?: Date;
+    declare updatedAt?: Date;
+
+}
+
+export function UserFactory(sequelize: Sequelize) {
+    User.init({
+        userName: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        createdAt: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        },
+        updatedAt: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        }
+    }, {
+        freezeTableName: true,
+        tableName: 'user',
+        sequelize
+    });
 }
