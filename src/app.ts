41ada5694099f2f1afec674ce463ccfdc3386a5a
Brian Niehausen <bniehausen@nbca.com>
2023-04-11 21:10:05 -0700

    installed cors in package.json

--- src/app.ts
+++ src/app.ts
@@ -29,16 +29,3 @@ db.sync({ alter: true }).then(() => {
 
 app.listen(3000);
 
-// import express, { Request, Response } from 'express'
-
-// const app = express();
-
-// const port = 3000
-
-// app.get('/', (req: Request, res: Response) => {
-//   res.send('Hello World!')
-// })
-
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`)
-// })
